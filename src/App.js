cd41236840a35b850d533a373c80b228c922a524
g <umacgamze49@gmail.com>
2021-06-28 21:06:13 +0200

    redux ist schon fertig und 4 Components ist erstellt

--- src/App.js
+++ src/App.js
@@ -1,47 +1,25 @@
-import React, { Component } from "react"
-import logo from "./logo.svg"
+import React, { Component } from "react";
 import "./App.css"
+import { BrowserRouter as Router,Switch, Route} from "react-router-dom"
+import Header from './containers/Header'
+import ProductListing from "./containers/ProductListing";
+import ProductDetail from "./containers/ProductDetail";
 
-class LambdaDemo extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { loading: false, msg: null }
-  }
-
-  handleClick = api => e => {
-    e.preventDefault()
-
-    this.setState({ loading: true })
-    fetch("/.netlify/functions/" + api)
-      .then(response => response.json())
-      .then(json => this.setState({ loading: false, msg: json.msg }))
-  }
-
-  render() {
-    const { loading, msg } = this.state
-
-    return (
-      <p>
-        <button onClick={this.handleClick("hello")}>{loading ? "Loading..." : "Call Lambda"}</button>
-        <button onClick={this.handleClick("async-dadjoke")}>{loading ? "Loading..." : "Call Async Lambda"}</button>
-        <br />
-        <span>{msg}</span>
-      </p>
-    )
-  }
-}
 
 class App extends Component {
   render() {
+    
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <LambdaDemo />
-        </header>
+      <Router>
+         <Header/>
+         <Switch>
+           <Route path="/" exact component={ProductListing}/>
+         <Route path="/product:productId" exact component={ProductDetail} />
+         <Route>404 Not Found!</Route>
+         </Switch>
+         
+      </Router>
       </div>
     )
   }
